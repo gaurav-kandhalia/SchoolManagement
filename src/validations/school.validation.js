6dd4b73d9f2a43cd208029384c7e17b2bfa8c3a8
Gaurav Kandhalia <vaidkandhala333@gmail.com>
2025-08-11 00:01:17 +0530

    validation with zod is completed

---
+++ src/validations/school.validation.js
@@ -0,0 +1,12 @@
+import {z} from 'zod';
+
+export const schoolSchema = z.object({
+    name: z.string().min(1, "School name is required"),
+    address: z.string().min(1, "Address is required"),
+    latitude: z.number().refine(val => Number.isFinite(val), {
+        message: "Latitude must be a valid floating number"
+    }),
+    longitude: z.number().refine(val => Number.isFinite(val), {
+        message: "Longitude must be a valid floating number"
+    })
+});
